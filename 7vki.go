e27e834a84ad3f8c85dd07fbe037259a54d805a1
CagriAlpat <cagri.alpat@gmail.com>
2023-08-05 13:17:59 +0300

    vucut-kitle-endeksi

---
+++ 7vki.go
@@ -0,0 +1,26 @@
+package main
+
+// //vucut kitle endeksi
+// // formul :vkı= kg/(boy*boy)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// )
+
+// func main() {
+// 	scanner := bufio.NewScanner(os.Stdin)
+
+// 	fmt.Print("kilonuzu giriniz ")
+// 	scanner.Scan()
+// 	kilo, _ := strconv.ParseFloat(scanner.Text(), 64)
+
+// 	fmt.Print("boyunuzu giriniz: ")
+// 	scanner.Scan()
+// 	boy, _ := strconv.ParseFloat(scanner.Text(), 64)
+
+// 	vki := kilo / (boy * boy)
+// 	fmt.Printf("vücüt kitele endeksizni = %f", vki)
+
+// }
