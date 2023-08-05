d0473f62331cd0403907881440bce2405915a8f9
CagriAlpat <140531199+CagriAlpat@users.noreply.github.com>
2023-08-05 13:36:03 +0300

    Update 7vki.go

--- 7vki.go
+++ 7vki.go
@@ -1,26 +1,26 @@
 package main
 
-// //vucut kitle endeksi
-// // formul :vkı= kg/(boy*boy)
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"os"
-// 	"strconv"
-// )
+ //vucut kitle endeksi
+ // formul :vkı= kg/(boy*boy)
+ import (
+ 	"bufio"
+ 	"fmt"
+ 	"os"
+ 	"strconv"
+ )
 
-// func main() {
-// 	scanner := bufio.NewScanner(os.Stdin)
+ func main() {
+ 	scanner := bufio.NewScanner(os.Stdin)
 
-// 	fmt.Print("kilonuzu giriniz ")
-// 	scanner.Scan()
-// 	kilo, _ := strconv.ParseFloat(scanner.Text(), 64)
+ 	fmt.Print("kilonuzu giriniz ")
+ 	scanner.Scan()
+ 	kilo, _ := strconv.ParseFloat(scanner.Text(), 64)
 
-// 	fmt.Print("boyunuzu giriniz: ")
-// 	scanner.Scan()
-// 	boy, _ := strconv.ParseFloat(scanner.Text(), 64)
+ 	fmt.Print("boyunuzu giriniz: ")
+ 	scanner.Scan()
+ 	boy, _ := strconv.ParseFloat(scanner.Text(), 64)
 
-// 	vki := kilo / (boy * boy)
-// 	fmt.Printf("vücüt kitele endeksizni = %f", vki)
+ 	vki := kilo / (boy * boy)
+ 	fmt.Printf("vücüt kitele endeksizni = %f", vki)
 
-// }
+ }
